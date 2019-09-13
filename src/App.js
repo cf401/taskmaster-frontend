cbce8f15dcf9ac9fd3a05a1d7b1acf8ab583fdf1
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-13 15:57:48 -0700

    working routes, working form on page, returns a thing.

--- src/App.js
+++ src/App.js
@@ -82,28 +82,27 @@ function History(props) {
 
 let form = new FormData();
 const APIPIC = 'http://taskmaster1-dev.us-west-2.elasticbeanstalk.com/api1/v2/tasks/images';
+const LOCAL = 'http://localhost:5000/api1/v2/tasks/images';
 
-function _handleChange(event) {
-  let value = event.target.files ? event.target.files[0] : event.target.value;
-  form.set(event.target.name, value);
-}
-
-function _upload(event) {
-  event.preventDefault();
-  fetch(APIPIC, {
-    method: "POST",
-    mode: 'no-cors',
-    body: form,
-  })
-  .then(response => response.json())
-  .catch(error => console.error('Error:', error))
-  .then(response => console.log('Success:', response));
-}
 
 function Forming(props){
+  function _handleChange(event) {
+    let value = event.target.files ? event.target.files[0] : event.target.value;
+    form.set(event.target.name, value);
+  }
+  function _upload(event) {
+    event.preventDefault();
+    fetch(APIPIC, {
+      method: "POST",
+      body: form,
+    })
+    .then(response => {console.log(response); return response.json()})
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
   return (
     <div>
-      <form onSubmit={_upload} action={API} method="post" encType="multipart/form-data">
+      <form onSubmit={_upload} method="post" encType="multipart/form-data">
         <label>
           <input onChange={_handleChange} name="title" type="text" placeholder="title" />
         </label>
