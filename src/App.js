1929e3d22d92a6c48f67c363186af9c368069684
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-13 14:51:35 -0700

    test flight;

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
           )
         })}
       </ul>
+      <Forming />
     </div>
   );
 }
@@ -78,5 +79,50 @@ function History(props) {
     </ol>
   )
 }
+
+let form = new FormData();
+
+const APIPIC = 'http://taskmaster1-dev.us-west-2.elasticbeanstalk.com/api1/v2/tasks/images';
+
+
+function _handleChange(event) {
+  let value = event.target.files ? event.target.files[0] : event.target.value;
+  form.set(event.target.name, value);
+}
+
+function _upload(event) {
+  event.preventDefault();
+  fetch(APIPIC, {
+    method: "POST",
+    mode: 'no-cors',
+    body: form,
+  })
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
+}
+
+function Forming(props){
+  return (
+    <div>
+      <form onSubmit={_upload} action={API} method="post" encType="multipart/form-data">
+        <label>
+          <input onChange={_handleChange} name="name" placeholder="name" />
+        </label>
+        <label>
+          <div>Are You Old?</div>
+          <input onChange={_handleChange} name="old" type="radio" value="true" /><span>Yes</span>
+          <input onChange={_handleChange} name="old" type="radio" value="false" /><span>No</span>
+        </label>
+        <label>
+          <span>Upload Image</span>
+          <input onChange={_handleChange} name="file" type="file" />
+        </label>
+        <button>Save</button>
+      </form>
+    </div>
+  );
+}
+
 
 export default App;
