9180a14e1d2d8917d2b04f11adf8c30eeb47af8b
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-19 16:38:23 -0700

    added display of pictures, title, assignee.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, {useState, useEffect} from 'react';
 
 import './App.css';
 
+//api for backend beanstalk
 const API = 'http://taskmaster-dev.us-west-2.elasticbeanstalk.com/api1/v2/tasks';
+//api for api gateway
+
 
 // On Load - Get that data from the API
 // Iterate and display major task points
@@ -42,9 +45,13 @@ function App() {
             <li key={task.id}>
               <details>
                 <summary>
-                  <span onClick={_deleteTask}>{task.title}</span>
+                  <span onClick={_deleteTask}>Title: {task.title}</span>
+                  <div>Assignee: {task.assignee}</div>
+                  <div>Description: {task.description}</div>
                 </summary>
                 <History history={task.historyList} />
+              
+              <img src={task.url} alt="task"></img>
               </details>
             </li>
           )
@@ -71,7 +78,7 @@ function History(props) {
       {props.history.map( (record,idx) => {
         return (
           <li key={idx}>
-            <span>{record.date}</span>
+            <span>{record.date}</span><br/>
             <span>{record.action}</span>
           </li>
         )
